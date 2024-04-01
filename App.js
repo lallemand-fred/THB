7d158a0144c4d08e9775ec43b8abc9a760c5935f
lallemand-fred <lallemand_fred@hotmail.com>
2024-04-01 23:46:38 +0200

    Chang : fichier styleCSS, index.html, App ajout d'animation de mouvement gauche, droit, bas, haut et opacité

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 
 
+// ----------------------------------Animations déplacement et blur-------------------------------
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entre) => {
+        console.log(entre)
+        if (entre.isIntersecting) {
+            entre.target.classList.add('show')
+        } else {
+            entre.target.classList.remove('show')
+        }
+    });
+})
 
+const slideElements = document.querySelectorAll('.slidG, .slidD, .slidB, .slidH, .slidO')
+slideElements.forEach((el) => observer.observe(el));
+
+// ----------------------------------Pages mouvement Réservation & contact-------------------------------
     var resBtn = document.getElementById("reservBtn");
     var contBtn = document.getElementById("contacterBtn");
     var res = document.getElementById("reserv");
@@ -21,3 +36,4 @@
     res.style.opacity = 0;
     cont.style.opacity = 1;
 }
+
